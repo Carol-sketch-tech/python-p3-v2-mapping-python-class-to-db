a4ff242497430c90dfab247281de77cbf8322f84
Caroline <bancroftwanjiru10000@gmail.com>
2024-09-17 13:15:13 +0300

    mapping python classes to database tables and mapping object instances to table rows, creating, deleting, and updating rowns in a database table

--- lib/debug.py
+++ lib/debug.py
@@ -5,5 +5,25 @@ from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create('payroll','Building A, 5th Floor')
+print(payroll)
+
+hr = Department.create("Human resource", "Building C, East Wing")
+print(hr)
+
+food = Department.create('Cafeteria', 'Building C, West wing, 1st Floor')
+print(food)
+
+hr.name = 'HR'
+hr.location = 'Building F, 10th Floor'
+hr.update()
+print(hr)
+
+print('Delete Payroll')
+payroll.delete()
+print(payroll)
 
 ipdb.set_trace()
--- lib/department.py
+++ lib/department.py
@@ -11,3 +11,69 @@ class Department:
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
  
+#  Adding ORM after existing methods.
+# creating table method.
+    @classmethod
+    def create_table(cls):
+        # create a new table to persist the attribute of the department isntances.
+        sql = '''
+                CREATE TABLE IF NOT EXISTS departments(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT)
+                '''
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    # delete table method drop_table(cls)
+    @classmethod
+    def drop_table(cls):
+        '''drop table that persists Deprtment instances'''
+        sql= '''
+            DROP TABLE IF EXISTS departments;
+        '''
+        CURSOR.execute(sql)
+        CONN.commit()
+
+   
+    def save(self):
+        # insert a new row with the name and location values of the current Department instance.
+        # Update object id attribute using the primary key value of new row.
+        sql = '''
+                INSERT INTO departments (name, location)
+                VALUES (?,?)
+                '''
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+    
+    @classmethod 
+    def create(cls, name, location):
+        # initializing a new department instance and save the object to the database.
+        department = cls(name, location) # here we have created a class instance.
+        department.save() # here we have called the save() method on the new instance creaetd.
+        return department
+    
+    def update(self):
+        # update the table row corresponding to the current department instance.
+        sql = '''
+                UPDATE departments
+                SET name = ?, location = ?
+                WHERE id = ?
+                '''
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+    
+    def delete(self):
+        #delete the table row corresponding to the current Depatment instance.
+        sql = '''
+                DELETE FROM departments
+                WHERE id = ?
+                '''
+        CURSOR.execute(sql, (self.id,))# the comman in the tuple is crucial for creating a tupel with one item.
+        CONN.commit()
+# we use bound parameters with each questionmark  bound to a value within the CURSOR.execute method call.
+
+
+    
